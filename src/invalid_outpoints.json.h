5e4596d9ddf1768e2289da4f69e785c446fbbb7b
Ultra Pool <37985843+ultra-pool@users.noreply.github.com>
2020-01-20 18:59:51 -0600

    github missed files

---
+++ src/invalid_outpoints.json.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2018 The sssolutions developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef sssolutions_INVALID_OUTPOINTS_JSON_H
+#define sssolutions_INVALID_OUTPOINTS_JSON_H
+#include <string>
+
+std::string LoadInvalidOutPoints()
+{
+    std::string str = "[]";
+    return str;
+}
+
+#endif //sssolutions_INVALID_OUTPOINTS_JSON_H
---
+++ src/invalid_serials.json.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 The sssolutions developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+
+#ifndef sssolutions_INVALID_SERIALS_JSON_H
+#define sssolutions_INVALID_SERIALS_JSON_H
+
+#include <string>
+
+std::string LoadInvalidSerials()
+{
+    std::string str ="[]";
+    return str;
+}
+
+#endif //sssolutions_INVALID_SERIALS_JSON_H
