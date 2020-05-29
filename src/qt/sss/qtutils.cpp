500f1935988b72efa79f24078c8a02296a0c6fcc
Liquid369 <Liquid369@gmail.com>
2020-05-29 12:01:38 -0500

    [GUI] Fix nav logo + default dark theme

--- src/qt/sss/qtutils.cpp
+++ src/qt/sss/qtutils.cpp
@@ -188,7 +188,7 @@ QSettings* getSettings()
 
 bool isLightTheme()
 {
-    return getSettings()->value("lightTheme", true).toBool();
+    return getSettings()->value("lightTheme", false).toBool();
 }
 
 void setTheme(bool isLight)
