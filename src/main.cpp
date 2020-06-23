d2c41a0b99ec828bf741f3f9807f0205f94b9c6a
Liquid369 <Liquid369@gmail.com>
2020-06-23 08:40:13 -0500

    Increase blocks in transit

--- src/main.cpp
+++ src/main.cpp
@@ -3695,13 +3695,10 @@ bool CheckWork(const CBlock block, CBlockIndex* const pindexPrev)
     if (block.nBits != nBitsRequired) {
         // sss Specific reference to the block with the wrong threshold was used.
         const Consensus::Params& consensus = Params().GetConsensus();
-        if (pindexPrev->nHeight + 1 <= (consensus.height_start_StakeModifierV2 + 21) || 
-            (pindexPrev->nHeight + 1 <= (uint32_t) consensus.nsssBadBlockBits)) {
+        if (block.nTime >= consensus.nsssBadBlockTime) {
             // accept SSS block minted with incorrect proof of work threshold
-            return true;
-        }
-
         return error("%s : incorrect proof of work at %d", __func__, pindexPrev->nHeight + 1);
+        }
     }
 
     return true;
